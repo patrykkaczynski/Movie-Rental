42a17266ac7d9c44e9a8982d7b85df3a36d60a37
patrykkaczynski <patrykkaczynski@proton.me>
2023-11-21 00:50:50 +0100

    Add migration to database after adding of configuration for rental and review entities

---
+++ src/MovieRental.Infrastructure/Migrations/20231120234559_AddReviewAndRentalConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MovieRental.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReviewAndRentalConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Formats",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "4K" },
+                    { 2, "Blu-ray" },
+                    { 3, "DVD" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Formats",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Formats",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Formats",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
